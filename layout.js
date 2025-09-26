14502e81725fb007c2beb9e67fd2f1eef01a112c
Avalon472 <mjakob478@gmail.com>
2025-09-26 17:53:29 -0500

    Added basic drawing functionality and grid clear

--- layout.js
+++ layout.js
@@ -1,5 +1,16 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = '#63aec5ff';
+
 const drawspace = document.getElementById("Drawspace")
-    for(i = 0 ; i<16; i++){
+const resetButton = document.getElementById("clearButton")
+
+let isMouseDown = false;
+document.body.addEventListener("mousedown", e => isMouseDown = true);
+document.body.addEventListener("mouseup", e => isMouseDown = false);
+
+resetButton.addEventListener("click", reset)
+function createGrid(size){
+    for(i = 0 ; i<size; i++){
         div = document.createElement("div");
         div.classList.toggle("row")
         for(j = 0; j<16; j++){
@@ -10,4 +21,22 @@ const drawspace = document.getElementById("Drawspace")
         }
         drawspace.appendChild(div);
     }
+    pixels = document.querySelectorAll(".pixel")
+    pixels.forEach(item => {
+        item.addEventListener("mouseover", colorIn)
+    })
+}
 
+function reset(){
+    drawspace.innerHTML = '';
+    createGrid(DEFAULT_SIZE);
+}
+
+function colorIn(e){
+    if(isMouseDown){
+    e.target.style.backgroundColor = "white";}
+}
+
+
+
+createGrid(DEFAULT_SIZE)
