f700dbe8e3c843341b6ad2a45f4e950587191e50
Avalon472 <mjakob478@gmail.com>
2025-10-03 17:22:28 -0500

    Added grid resizing and fixed drawing issues

--- layout.js
+++ layout.js
@@ -1,42 +1,79 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = '#63aec5ff';
 
+let currentSize = DEFAULT_SIZE
+let currentColor = DEFAULT_COLOR
+
 const drawspace = document.getElementById("Drawspace")
 const resetButton = document.getElementById("clearButton")
+const borderButton = document.getElementById("borderToggle")
+const sizeSlider = document.getElementById("gridSize")
+const sizeText = document.getElementById("sizeText")
 
 let isMouseDown = false;
 document.body.addEventListener("mousedown", e => isMouseDown = true);
 document.body.addEventListener("mouseup", e => isMouseDown = false);
 
 resetButton.addEventListener("click", reset)
-function createGrid(size){
-    for(i = 0 ; i<size; i++){
-        div = document.createElement("div");
-        div.classList.toggle("row")
-        for(j = 0; j<16; j++){
-            div2 = document.createElement("div");
-            div2.classList.toggle("pixel");
-            div2.classList.toggle("column")
-            div.appendChild(div2)
-        }
-        drawspace.appendChild(div);
+sizeSlider.addEventListener("input", e => resizeGrid(parseInt(e.target.value)))
+borderButton.addEventListener("click", toggleBorder)
+
+function createGrid(){
+
+    let size = currentSize;
+
+    drawspace.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    drawspace.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i< size*size; i++){
+        const pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        pixel.classList.toggle("hasBorder")
+        pixel.addEventListener("mouseover", colorIn)
+        pixel.addEventListener("mousedown", colorIn)
+        drawspace.appendChild(pixel)
     }
-    pixels = document.querySelectorAll(".pixel")
-    pixels.forEach(item => {
-        item.addEventListener("mouseover", colorIn)
-    })
 }
 
 function reset(){
     drawspace.innerHTML = '';
-    createGrid(DEFAULT_SIZE);
+    createGrid();
 }
 
 function colorIn(e){
-    if(isMouseDown){
-    e.target.style.backgroundColor = "white";}
+    if(e.type == "mousedown" || (e.type == "mouseover" && isMouseDown)){
+    e.target.style.backgroundColor = currentColor;}
 }
 
+function resizeGrid(size){
+    currentSize = size;
+    reset();
+    sizeText.textContent = `${size} x ${size}`;
+}
+
+function toggleBorder(){
+    allPixels = document.querySelectorAll(".pixel")
+    allPixels.forEach(element => {
+        element.classList.toggle("hasBorder")
+    });
+}
+
+createGrid()
 
 
-createGrid(DEFAULT_SIZE)
+    // for(i = 0 ; i<currentSize; i++){
+    //     div = document.createElement("div");
+    //     div.classList.toggle("row")
+    //     for(j = 0; j<currentSize; j++){
+    //         div2 = document.createElement("div");
+    //         div2.classList.toggle("pixel");
+    //         div2.classList.toggle("column")
+    //         div.appendChild(div2)
+    //     }
+    //     drawspace.appendChild(div);
+    // }
+        // pixels = document.querySelectorAll(".pixel")
+        // pixels.forEach(item => {
+        // item.addEventListener("mouseover", colorIn)
+        // item.addEventListener("mousedown", colorIn)
+        // })
